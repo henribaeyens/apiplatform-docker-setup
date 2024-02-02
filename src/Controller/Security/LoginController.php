6eeeb8e0c4e206952935098c8eee93b4a8ab6418
Henri Baeyens <henri.baeyens@gmail.com>
2024-02-03 00:34:05 +0100

    authenticate user upon verification

--- src/Controller/Security/LoginController.php
+++ src/Controller/Security/LoginController.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace App\Controller\Security;
 
 use Sonata\AdminBundle\Admin\Pool;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -27,12 +29,12 @@ final class LoginController extends AbstractController
     }
 
     #[Route('/admin/login', name: 'admin_login')]
-    public function __invoke(Request $request): Response
+    public function __invoke(Request $request, #[CurrentUser] ?UserInterface $user): Response|RedirectResponse
     {
-        if ($this->isAuthenticated()) {
+        if (null !== $user) {
             $this->addFlash(
                 'error',
-                $this->translator->trans('security.error.user_already_authenticated')
+                $this->translator->trans('flash.error.user_already_authenticated')
             );
             return $this->redirectToRoute('sonata_admin_dashboard');
         }
@@ -49,10 +51,5 @@ final class LoginController extends AbstractController
             'last_username' => $this->authenticationUtils->getLastUsername(),
             'reset_route' => $this->generateUrl('admin_password_request'),
         ]);
-    }
-
-    private function isAuthenticated(): bool
-    {
-        return $this->getUser() instanceof UserInterface;
     }
 }
